6731e1227427e3b05efcfc848a127167f1db11f8
jacquarg <guillaume.jacquart@hoodbrains.com>
2017-03-15 15:32:11 +0100

    Add fontawesome. Add a title to the library. Move playe above track list.

--- app/views/movie_library.js
+++ app/views/movie_library.js
@@ -2,11 +2,27 @@
 
 const MovieItemView = require('./movie_item');
 const emptyViewTemplate = require('./templates/movie_library_empty');
+const template = require('./templates/my_movies');
 
-
-module.exports = Mn.CollectionView.extend({
+const MovieLibraryView = Mn.CollectionView.extend({
   tagName: 'ul',
   className: 'movielibrary',
   childView: MovieItemView,
   emptyView: Mn.View.extend({ template: emptyViewTemplate, }),
+});
+
+module.exports = Mn.View.extend({
+  className: 'mymovies',
+  template: template,
+
+  regions: {
+    collection: {
+      el: 'ul',
+      replaceElement: true,
+    },
+  },
+
+  onRender: function () {
+    this.showChildView('collection', new MovieLibraryView({ collection: this.collection }));
+  },
 });
