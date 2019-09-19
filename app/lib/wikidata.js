c0680f97cd93f068fa2f7ed0f8d3b6f1a681536e
Cezary <ziemlicki@gmail.com>
2019-09-19 10:43:57 +0200

    pushed on Orange Git

--- app/lib/wikidata.js
+++ app/lib/wikidata.js
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2018 - 2019 Orange
+ * 
+ * This software is distributed under the terms and conditions of the 'MIT'
+ * license which can be found in the file 'LICENSE.txt' in this package distribution 
+ * or at https://spdx.org/licenses/MIT
+ *
+ */
+
+ /* Orange contributed module for use on CozyCloud platform
+ * 
+ * Module name: LMDMF - La musique de mes films
+ * Version:     3.0.13
+ * Created:     2018 by Orange
+ */
+
+
 'use-strict';
 
 const WalkTreeUtils = require('./walktree_utils');
--- app/versionsmigrations.js
+++ app/versionsmigrations.js
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2018 - 2019 Orange
+ * 
+ * This software is distributed under the terms and conditions of the 'MIT'
+ * license which can be found in the file 'LICENSE.txt' in this package distribution 
+ * or at https://spdx.org/licenses/MIT
+ *
+ */
+
+ /* Orange contributed module for use on CozyCloud platform
+ * 
+ * Module name: LMDMF - La musique de mes films
+ * Version:     3.0.13
+ * Created:     2018 by Orange
+ */
+
+
 module.exports.runMigration = (previous) => { // previous, current
   // TODO : it's ugly, can we factoryse it ?
   if (previous < '3.0.z' && Number(previous.slice(4)) < 12) {
